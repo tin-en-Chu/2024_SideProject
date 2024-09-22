3480ae94725961f37a30af586f8455eae71d44fe
kyle <bobsteven353200@gmail.com>
2024-09-22 20:20:20 +0800

    first commit git

---
+++ src/main/java/com/example/test/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.example.test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/Hello")
+    public String Hello(@RequestParam String str){
+        System.out.println(str);
+        return "Helloooodddddddddd";
+    }
+}
